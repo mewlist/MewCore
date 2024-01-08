5718be1771faad0ebd80f691a4229a57175a13e9
mewlist <mewlist@mewlist.com>
2024-01-08 15:50:51 +0900

    SceneManagerHelper

---
+++ Runtime/Utils/SceneManagerHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace Mew.Core.SceneUtils
+{
+    public static class SceneManagerHelper
+    {
+        public static List<Scene> GetLoadedScenes()
+        {
+            var loadedSceneCount = SceneManager.loadedSceneCount;
+            var loadedScenes = new List<Scene>();
+            for (var i=0; i<loadedSceneCount; i++)
+                loadedScenes.Add(SceneManager.GetSceneAt(i));
+            return loadedScenes;
+        }
+    }
+}
