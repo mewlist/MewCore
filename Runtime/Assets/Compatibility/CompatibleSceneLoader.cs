d327e16545b96ed6d28d059f87b442b69674c8e1
mewlist <mewlist@mewlist.com>
2024-02-18 16:16:44 +0900

    UniTask Integration

--- Runtime/Assets/Compatibility/CompatibleSceneLoader.cs
+++ Runtime/Assets/Compatibility/CompatibleSceneLoader.cs
@@ -3,17 +3,25 @@ using Mew.Core.TaskHelpers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if USE_UNITASK
+using Cysharp.Threading.Tasks;
+#endif
+
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
+
 namespace Mew.Core.Assets
 {
     internal static class CompatibleSceneLoader
     {
         internal static async ValueTask<SceneHandle> LoadSceneAsync(UnifiedScene unifiedScene, LoadSceneParameters parameters)
         {
-#if UNITY_2023_2_OR_NEWER
+#if UNITY_2023_2_OR_NEWER || USE_UNITASK
             await SceneManager.LoadSceneAsync(unifiedScene.SceneReference, parameters);
 #else
-                var asyncOp = SceneManager.LoadSceneAsync(unifiedScene.SceneReference, parameters);
-                while (!asyncOp.isDone) await TaskHelper.NextFrame();
+            var asyncOp = SceneManager.LoadSceneAsync(unifiedScene.SceneReference, parameters);
+            while (!asyncOp.isDone) await TaskHelper.NextFrame();
 #endif
             var loadedScene = SceneManager.GetSceneAt(SceneManager.loadedSceneCount - 1);
             return new SceneHandle(loadedScene);
@@ -21,12 +29,27 @@ namespace Mew.Core.Assets
 
         internal static async ValueTask UnloadSceneAsync(SceneHandle sceneHandle)
         {
-#if UNITY_2023_2_OR_NEWER
+#if UNITY_2023_2_OR_NEWER || USE_UNITASK
             await SceneManager.UnloadSceneAsync(sceneHandle.Scene);
 #else
             var asyncOp = SceneManager.UnloadSceneAsync(sceneHandle.Scene);
             while (!asyncOp.isDone) await TaskHelper.NextFrame();
 #endif
         }
+
+#if UNITY_EDITOR
+        public static async Task<ISceneHandle> LoadEditorSceneAsync(UnifiedScene unifiedScene, LoadSceneParameters parameters)
+        {
+
+#if UNITY_2023_2_OR_NEWER || USE_UNITASK
+            await EditorSceneManager.LoadSceneAsyncInPlayMode(unifiedScene.EditorScenePath , parameters ) ;
+#else
+            var asyncOp = EditorSceneManager.LoadSceneAsyncInPlayMode(unifiedScene.EditorScenePath , parameters ) ;
+            while (!asyncOp.isDone) await TaskHelper.NextFrame();
+#endif
+            var loadedScene = SceneManager.GetSceneAt(SceneManager.loadedSceneCount - 1);
+            return new SceneHandle(loadedScene);
+        }
+#endif
     }
 }
